aa0a4f6485ade9a3e6cfc7ead8a5b14b5b29ac99
D630 <d630@posteo.net>
2015-10-19 04:09:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,109 @@
+##### README
+
+[bma](https://github.com/D630/bma) is a simple way to manage bookmarks of directories with the hash builtin command of GNU bash.
+
+##### BUGS & REQUESTS
+
+Please feel free to open an issue or put in a pull request on https://github.com/D630/bma
+
+##### GIT
+
+To download the very latest source code:
+
+```
+git clone https://github.com/D630/bma
+```
+
+In order to use the latest tagged version, do also something like this:
+
+```
+cd -- ./bma
+git checkout $(git describe --abbrev=0 --tags)
+```
+
+##### INSTALL
+
+Put the following to your `~/.bashrc`:
+
+```
+source FILENAME/OF/BMA && __bma -i
+```
+
+The above line will setup:
+- three functions: `__bma`, `bcd` and `_bma_completion`
+- one binding: `Ctrl-x b` to `bcd`
+- three alias: `bb`, `bma` and `bs`
+- one name completion for `__bma`, `bma` and `bb`
+- one empty BMARKS_INDEX_FILE
+
+bcd makes use of [fzf](https://github.com/junegunn/fzf). Modify it, when you do not wanna use it.
+
+##### USAGE
+
+```
+__bma -[acdihlps]
+```
+
+###### ENVIRONMENT VARIABLES
+
+```
+BMARKS_INDEX_FILE       ${BMARKS_INDEX_FILE:-${XDG_DATA_HOME}/bmarks.txt}
+```
+
+###### OPTIONS
+
+```
+-a PATHNAME BMARK       Add bookmark and update index file.
+-c BMARK                Cd into bookmark.
+-d BMARK                Remove boomark from the index file.
+-h                      Print this help.
+-i                      Initialize index file.
+-l                      Execute 'hash' and filter bookmarks.
+-p                      Print all pathnames and bookmarks.
+-s                      Select bookmarks via the select compound
+                        command of GNU bash.
+```
+
+###### EXAMPLES
+
+```sh
+@ cd $HOME
+@ bma -a $PWD home
+added '/home/user1' as 'home'
+@ cd / && echo $PWD
+/
+@ bb home
+/home/user1
+@ bma -p
+/home/user1 home
+@ cd /
+@ bms
+1) home
+cd -- 1
+/home/user1
+@ bma -d home
+index file seems to be empty now
+```
+
+###### INDEX FILE
+
+The index file looks like this:
+
+```sh
+builtin hash -p /home/user1/.BMARKS home
+builtin hash -p /home/user1/tmp/.BMARKS tmp
+```
+
+##### NOTICE
+
+bma has been written in [GNU bash](http://www.gnu.org/software/bash/) on [Debian GNU/Linux 9 (stretch/sid)](https://www.debian.org) using these programs/packages:
+
+- GNU bash 4.3.42(1)-release
+- GNU coreutils 8.23: sort
+- GNU grep 2.21
+- GNU sed 4.2.2
+- fzf 0.10.8
+
+##### LICENCE
+
+GNU GPLv3
---
+++ bma.bash
@@ -0,0 +1,163 @@
+#!/usr/bin/env bash
+
+# bma [GNU GPLv3]
+# https://github.com/D630/bma
+
+__bma ()
+{
+        builtin typeset \
+                BMARKS_INDEX_FILE=${BMARKS_INDEX_FILE:-${$XDG_DATA_HOME}/bmarks.txt}
+
+        [[ -f $BMARKS_INDEX_FILE ]] || > "$BMARKS_INDEX_FILE" || builtin return 1
+
+        case $1 in
+        -a)
+                if
+                        [[ -n $2 && -n $3 ]]
+                then
+                        if
+                                builtin hash -p "${2}/.BMARKS" -- "$3"
+                        then
+                                if
+                                        builtin hash -l \
+                                        | command fgrep '/.BMARKS ' > "$BMARKS_INDEX_FILE"
+                                then
+                                        builtin printf '%s\n' "added '$2' as '$3'" 1>&2
+                                else
+                                        builtin printf '%s\n' "error: could not update index file" 1>&2
+                                fi
+                        else
+                                builtin printf '%s\n' "error: could not add '$3'" 1>&2
+                        fi
+                else
+                        builtin printf '%s\n' "error: bad arguments: '$*'" 1>&2
+                fi
+        ;;
+        -c)
+                if
+                        [[ -n $2 ]]
+                then
+                        builtin typeset b="$(builtin hash -t "$2")"
+                        if
+                                builtin cd -- "${b%/.BMARKS}"
+                        then
+                                builtin printf '%s\n' "$PWD" 1>&2
+                        else
+                                builtin printf '%s\n' "error: could not cd into '$2'" 1>&2
+                        fi
+                else
+                        builtin printf '%s\n' "error: bad arguments: '$*'" 1>&2
+                fi
+        ;;
+        -d)
+                if
+                        builtin typeset p="$(
+                                __bma -p \
+                                | command grep " ${2}$"
+                        )"
+                        builtin hash -d "$2"
+                then
+                        if
+                                builtin hash -l \
+                                | command fgrep '/.BMARKS ' > "$BMARKS_INDEX_FILE"
+                        then
+                                builtin printf '%s\n' "removed '$2' (${p% *})" 1>&2
+                        else
+                                builtin printf '%s\n' "index file seems to be empty now" 1>&2
+                        fi
+                else
+                        builtin printf '%s\n' "error: could not remove '$2'" 1>&2
+                fi
+        ;;
+        -i)
+                builtin source "$BMARKS_INDEX_FILE" || {
+                        builtin printf '%s\n' "error: could not source '$BMARKS_INDEX_FILE'" 1>&2
+                }
+        ;;
+        -l)
+                builtin hash \
+                | command sed -n '/\/\.BMARKS$/ s/\/\.BMARKS$// p'
+        ;;
+        -p)
+                command sed -n 's/\/\.BMARKS//;s/builtin hash -p // p' "$BMARKS_INDEX_FILE"
+        ;;
+        -s)
+                builtin typeset \
+                        PS3='cd -- ' \
+                        i;
+                select i in $(__bma -p | command sed 's/.* //' | command sort)
+                do
+                        __bma -c "$i" && builtin break
+                done
+        ;;
+        -h)
+                { builtin typeset help="$(</dev/fd/0)" ; } <<'HELP'
+Usage
+        __bma -[acdihlps]
+
+Options
+        -a PATHNAME BMARK       Add bookmark and update index file.
+        -c BMARK                Cd into bookmark.
+        -d BMARK                Remove boomark from the index file.
+        -h                      Print this help.
+        -i                      Initialize index file.
+        -l                      Execute 'hash' and filter bookmarks.
+        -p                      Print all pathnames and bookmarks.
+        -s                      Select bookmarks via the select compound command
+                                of GNU bash.
+HELP
+        builtin printf '%s\n' "$help"
+        ;;
+        *)
+                builtin printf '%s\n' "usage: __bma -[acdhilps]" 1>&2
+        esac
+}
+
+bcd ()
+{
+        builtin typeset dir
+        builtin read -r dir _ < <(
+                __bma -p \
+                | command fzf
+        )
+
+        if
+                [[ -d $dir ]]
+        then
+                builtin cd -- "$dir" && builtin printf '%s\n' "$PWD" 1>&2
+        else
+                builtin printf '%s\n' "no dir has been chosen" 1>&2
+                builtin return 1
+        fi
+}
+
+builtin bind -x '"\C-xb": bcd'
+
+_bma_completion ()
+{
+        builtin typeset \
+                BMARKS_INDEX_FILE=${BMARKS_INDEX_FILE:-${XDG_DATA_HOME}/bmarks.txt} \
+                cur=${COMP_WORDS[COMP_CWORD]};
+
+        builtin typeset -a dirs="()"
+
+        [[ -f $BMARKS_INDEX_FILE ]] || builtin return 1
+
+        builtin mapfile -t dirs < <(
+                command sed 's/.* //' "$BMARKS_INDEX_FILE"
+        )
+
+        builtin mapfile -t COMPREPLY < <(
+                builtin compgen -W '${dirs[@]}' -- "$cur"
+        )
+
+        builtin return 0
+}
+
+builtin complete -F _bma_completion -- __bma bma bb
+
+alias bb='__bma -c'
+alias bma=__bma
+alias bs='__bma -s'
+
+# vim: set ts=8 sw=8 tw=0 et :
